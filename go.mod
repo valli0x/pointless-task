50d68fc4c6f70b16f46ca86a075d09903fc2ec45
valli0x <youarethesame1821@gmail.com>
2024-02-08 18:46:43 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test/pointless-task
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const (
+	packetSize = 10
+	numWorkers = 3
+
+	envInterval = "N"
+	envWorkers  = "M"
+	envDuration = "K"
+)
+
+func main() {
+	interval, _ := strconv.Atoi(os.Getenv(envInterval))
+	workers, _ := strconv.Atoi(os.Getenv(envWorkers))
+	duration, _ := strconv.Atoi(os.Getenv(envDuration))
+
+	packets := make(chan []int) // Канал для передачи пакетов данных
+
+	// Packet Generator
+	go func() {
+		for {
+			packet := generatePacket()
+			packets <- packet
+			time.Sleep(time.Millisecond * time.Duration(interval)) // Ожидание перед генерацией следующего пакета
+		}
+	}()
+
+	// Workers
+	results := make(chan []int, 100) // Канал для передачи результатов обработки пакетов
+
+	wg := &sync.WaitGroup{}
+	wg.Add(workers)
+
+	for i := 0; i < workers; i++ {
+		go func() {
+			for packet := range packets {
+				result := processPacket(packet)
+				results <- result // Отправка результата в канал для аккумуляции
+			}
+			wg.Done()
+		}()
+	}
+
+	// Accumulator
+	go func() {
+		var accumulator int
+		for result := range results {
+			accumulator += sum(result)
+			fmt.Println("accumulator:", accumulator)
+			time.Sleep(time.Second * time.Duration(duration)) // Ожидание указанной длительности
+		}
+	}()
+
+	close(packets)
+	wg.Wait()
+	close(results)
+}
+
+func generatePacket() []int {
+	packet := make([]int, packetSize)
+	for i := 0; i < packetSize; i++ {
+		packet[i] = rand.Intn(10)
+	}
+	return packet
+}
+
+func processPacket(packet []int) []int {
+	sort.Slice(packet, func(i, j int) bool {
+		return packet[i] > packet[j]
+	})
+	return packet[:3]
+}
+
+func sum(ints []int) int {
+	result := 0
+	for _, num := range ints {
+		result += num
+	}
+	return result
+}
